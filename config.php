b972beae6b9abfea60561b31d3e69359a9ebc07f
DD <68047131+xF346x@users.noreply.github.com>
2020-08-14 00:48:20 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+/* Config adzec.cc */
+
+$keys = "gFyoqNPZpJUQLMKBYWIzXBMMZOINSZ"; // Ambil Disini => https://cb.run/ypcd
+$useragent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/603.2.5 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.5";
+$phpssid = "sl08ahbc02o8vta7pu3519bko2";
+$token = "token_QpUJAAAAAAAAGu98Hdz1l_lcSZ2rY60Ajjk9U1c=BAYAXx7M8wFfHv6wgAGBAsAAIKyFgPe-MhE4wL6hknqdlgS64lahpN8t_wTlPZKGwh3UwQBHMEUCIQDVkkJeDCiZTyXzZuJ8WuJsaparEV3M4FYRpDX5F1onRwIgMeAA35Yw6XBij9qDDguNkRT7GHEuX7h0UCVrC7eQCow";
+$valsurf = "SAsurf=SAsurfVal";
